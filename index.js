8799d14fdc9cca226a051e698c83d79d9fdb95bf
Brandon Cunningham <brandido@gmail.com>
2018-07-23 15:37:33 -0600

    Functianal playground

--- index.js
+++ index.js
@@ -1,51 +1,83 @@
-var title = $('#title-input').val();
-var body = $('#body-input').val();
+// var title = $('#title-input').val();
+// var body = $('#body-input').val();
 var numCards = 0;
-var qualityVariable = "swill";
+// var qualityVariable = "swill";
 
-var newCard = function(id , title , body , quality) {
-    return '<div id="' + id + '"class="card-container"><h2 class="title-of-card">'  
-            + title +  '</h2>'
-            + '<button class="delete-button"></button>'
-            +'<p class="body-of-card">'
-            + body + '</p>'
-            + '<button class="upvote"></button>' 
-            + '<button class="downvote"></button>' 
-            + '<p class="quality">' + 'quality:' + '<span class="qualityVariable">' + quality + '</span>' + '</p>'
-            + '<hr>' 
-            + '</div>';
+// $(document).ready(newCard());
+// passed object in as paramenter and onto values
+    // prepending to DOM - not working
+function addTodoItemToDom(obj) {
+console.log(obj);
+
+    $('.bottom-box').append(`
+      <li id="${obj.id}" class="card-container">
+        <h2 class="title-of-card"> ${obj.title} </h2>
+        <button class="delete-button"></button>
+        <p>${obj.body}</p>
+        <button class="upvote"></button>
+        <button class="downvote"></button>
+        <p class="quality">quality:<span class="qualityVariable">${obj.quality}</span></p>
+        <hr> 
+      </li>`);
 };
 
-function cardObject() {
-    return {
-        title: $('#title-input').val(),
-        body: $('#body-input').val(),
-        quality: qualityVariable
+// reformat to a constructor function with defined parameters
+function cardObject(id, title, body, quality) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.quality = quality;
     };
-}
+
+    cardObject();
+
+    function somethingOnLoad() {
+
+
+        // put up an array and use that. 
+// if something in local storage, get it parse it and run it the function that appends to dom. 
+
+// if theres nothing in local storage do nothing. 
+
+
+    }
+
+    somethingOnLoad();
 
 $.each(localStorage, function(key) {
-    var cardData = JSON.parse(this);
+    var objectFromStorage = localStorage.getItem(key);
+    var cardData = JSON.parse(objectFromStorage);
     numCards++;
-    $( ".bottom-box" ).prepend(newCard(key, cardData.title, cardData.body, cardData.quality));
+    // $( ".bottom-box" ).prepend(addTodoItemToDom(key, cardData.title, cardData.body, cardData.quality));
+    addTodoItemToDom(cardData);
 });
 
-var localStoreCard = function() {
-    var cardString = JSON.stringify(cardObject());
+function localStoreCard (cardObject) {
+    var cardString = JSON.stringify(cardObject);
+    console.log(cardObject);
+    console.log(cardString);
     localStorage.setItem('card' + numCards  , cardString);
 }
 
-$('.save-btn').on('click', function(event) {
-    event.preventDefault();
-    if ($('#title-input').val() === "" || $('#body-input').val() === "") {
-       return false;
-    };  
+$('.save-btn').on('click', saveButton)
 
+function saveButton (event) {
+    event.preventDefault();
+    // console.log($('#title-input').val())
+    // if ($('#title-input').val() === "" || $('#body-input').val() === "") {
+    //    return false;
+    // };  
+    // changed Card to cardObject, added keyword new
+    var ideaObject = new cardObject(Date.now(), $('#title-input').val(), $('#body-input').val(), 'swill')
+    // console.log(ideaObject)
     numCards++;
-    $( ".bottom-box" ).prepend(newCard('card' + numCards, $('#title-input').val(), $('#body-input').val(), qualityVariable)); 
-    localStoreCard();
+    // $( ".bottom-box" ).prepend(newCard('card' + numCards, $('#title-input').val(), $('#body-input').val(), 'swill')); 
+    //passing cardObject into localStoreCard
+    localStoreCard(ideaObject);
+    //passing cardObject into newCard to append to DOM
+    addTodoItemToDom(ideaObject);
     $('form')[0].reset();
-});
+};
 
 $(".bottom-box").on('click', function(event){
     var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
